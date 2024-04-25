5ac23f9719740b5d39b1761faafbc021b4cae69a
Dhiraj <dhirajsase07@gmail.com>
2024-04-25 08:11:28 +0530

    Initial Commit

---
+++ REST_tut/REST_tut/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const PORT = 4500;
+
+app.use(express.json());
+
+const loginModel = require("./db");
+
+app.get("/", async (req, resp) => {
+  const result = await loginModel.find();
+  resp.send(result);
+});
+
+app.post("/", async (req, resp) => {
+  const data = new loginModel(req.body);
+  console.log(data);
+  const result = await data.save();
+  resp.send(result);
+});
+
+app.patch("/:id", async (req, resp) => {
+  const result = await loginModel.updateOne(
+    { _id: req.params.id },
+    { $set: { name: "vikas" } }
+  );
+  resp.send(result);
+});
+
+app.delete("/:id", async (req, resp) => {
+  const result = await loginModel.deleteOne({ _id: req.params.id });
+  resp.send(result);
+});
+
+app.listen(PORT, () => {
+  console.log("your server is ready");
+});
